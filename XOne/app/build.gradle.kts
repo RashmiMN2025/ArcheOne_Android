0c29e591bdcf14a2e580355f3e2b0e3146f215d5
Pramukh JS <netconncmp@netcon.in>
2025-07-03 18:16:10 +0530

    03-July

--- XOne/app/build.gradle.kts
+++ XOne/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.archeGlobal.one"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.0"
+        versionCode = 9
+        versionName = "1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
             abiFilters += listOf("armeabi", "armeabi-v7a", "arm64-v8a", "x86", "x86_64")
