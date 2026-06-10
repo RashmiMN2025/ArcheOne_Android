d374505deeb08f4bdc32fa053127781045c81841
RashmiMN2025 <rashmi.mn@arche.global>
2026-06-10 11:01:53 +0530

    SSL_certificate_change

--- XOne/app/build.gradle.kts
+++ XOne/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.archeGlobal.one"
         minSdk = 26
         targetSdk = 35
-        versionCode = 29
-        versionName = "1.10"
+        versionCode = 30
+        versionName = "1.11"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
             abiFilters += listOf("armeabi", "armeabi-v7a", "arm64-v8a", "x86", "x86_64")
